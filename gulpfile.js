c5dd3c8d24c3f9a0694532c7abb739740a3d4be7
Stephen Poole <stephen.poole@redlioncanada.com>
2016-07-27 14:10:34 -0400

    Completed banner tests implementation. Separated components into their own files. Objectified options and added defaults.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
 	rename = require('gulp-rename'),
-	validator = require('./')
+	adwords = require('./')
 
 gulp.task('default', function() {
 	gulp.src('./build/compiled/test.compiled/**/*')
-		.pipe(validator({verbose:false}))
+		.pipe(adwords({verbose:true, filesize:150, name:'Test1', environment:'adwords'}))
 })
