c3a9ddf9028f0ac971c7099fe99377a779580280
Stephen Poole <stephen.poole@redlioncanada.com>
2016-08-09 10:39:02 -0400

    Added option to add custom tests

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp'),
 	adwords = require('../index')
 
+var tests = [
+	{
+		test: function(html, files) {
+			return false
+		},
+		message: 'YOU LOSE!'
+	}
+]
+
 gulp.task('default', function() {
 	//will pass
 	gulp.src('./banners/passed/**/*')
-		.pipe(adwords({verbose:false, filesize:150, name:'Passing Banner', environment:'adwords'}))
+		.pipe(adwords({verbose:false, filesize:150, name:'Passing Banner', environment:'adwords',customTests:tests}))
 
 	//environment won't match
 	gulp.src('./banners/environment_mismatch/**/*')
