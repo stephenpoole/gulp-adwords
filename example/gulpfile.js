ce3642093199d1e42831e0fe9dc8f0ebf6b0fa9a
Stephen Poole <stephenwpoole@gmail.com>
2018-07-06 17:23:58 -0400

    Added additional context to errors. Removed verbose option.
    Reformatted log output. Now transpiling from es6 via webpack and serving that file.

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,26 +1,34 @@
-var gulp = require('gulp'),
-	adwords = require('../index')
+var adwords = require('../');
+var gulp = require('gulp');
 
 var tests = [
-	{
-		test: function(html, files) {
-			return false
-		},
-		message: 'YOU LOSE!',
-		name: 'LOSING_TEST'
-	}
-]
+    {
+        test: function(html, files) {
+            return false;
+        },
+        message: 'YOU LOSE!',
+        name: 'LOSING_TEST'
+    }
+];
 
 gulp.task('default', function() {
-	//will pass
-	gulp.src('./banners/passed/**/*')
-		.pipe(adwords({filesize:150, name:'Passing Banner', environment:'adwords'}))
+    //will pass
+    gulp.src('./banners/passed/**/*').pipe(
+        adwords({
+            filesize: 150,
+            name: 'Passing Banner',
+            environment: 'adwords'
+        })
+    );
 
-	//environment won't match
-	gulp.src('./banners/environment_mismatch/**/*')
-		.pipe(adwords())
+    // environment won't match
+    gulp.src('./banners/environment_mismatch/**/*').pipe(adwords());
 
-	//custom test
-	gulp.src('./banners/passed/**/*')
-		.pipe(adwords({name: 'Custom Test Banner', customTests: tests}))
-})
+    // multiple errors
+    gulp.src('./banners/multiple/**/*').pipe(adwords());
+
+    // custom test
+    gulp.src('./banners/passed/**/*').pipe(
+        adwords({ name: 'Custom Test Banner', customTests: tests })
+    );
+});
